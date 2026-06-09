0f430248ec91ab35134298ae84b17b08f5fb1573
Sebastien Marinier <sebastien.marinier@arawa.fr>
2026-06-09 10:07:17 +0200

    fix: extend validateBool to 1 value

--- lib/Config.php
+++ lib/Config.php
@@ -5,32 +5,38 @@ namespace OCA\Matomo;
 use OCA\Matomo\AppInfo\Application;
 use OCP\IAppConfig;
 
-class Config {
-	public function __construct(private IAppConfig $config) {
+class Config
+{
+	public function __construct(private IAppConfig $config)
+	{
 	}
 
-	public function getAppValue(string $key, string $default = ''): string {
+	public function getAppValue(string $key, string $default = ''): string
+	{
 		$value = $this->config->getValueString(Application::ID, $key, $default);
 		return (empty($value)) ? $default : $value;
 	}
 
-	public function setAppValue(string $key, string $value) {
+	public function setAppValue(string $key, string $value)
+	{
 		if (in_array($key, ['trackDir', 'trackUser'])) {
 			return $this->config->setValueBool(Application::ID, $key, $this->validateBoolean($value));
 		}
 		return $this->config->setValueString(Application::ID, $key, $value);
 	}
 
-	public function getBooleanAppValue($key) {
+	public function getBooleanAppValue($key)
+	{
 		return $this->config->getValueBool(Application::ID, $key);
 	}
 
-	private function validateBoolean($val) {
-		return $val === true || $val === 'true';
+	private function validateBoolean($val)
+	{
+		return $val === true || $val === 'true' || $val === 1 || $val === '1';
 	}
 
-	public function deleteAppValue(string $key) {
+	public function deleteAppValue(string $key)
+	{
 		return $this->config->deleteKey(Application::ID, $key);
 	}
-
 }
